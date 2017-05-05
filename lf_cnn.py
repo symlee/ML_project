222bc663d1ed3a7f76a07b4d731844b99d65c811
vnganesh <vnganesh@andrew.cmu.edu>
2017-05-04 22:13:32 -0400

    incorporated cormac's changes

--- lf_cnn.py
+++ lf_cnn.py
@@ -17,11 +17,11 @@ import cv2
 import numpy as np
 
 # parameters
-batch_size = 32
+batch_size = 64
 num_classes = 2
 epochs = 12
 
-train_val_ratio = 0.5
+train_val_ratio = 1 # percentage used for training
 n = 30   # number of images per chunk (current works at around 100, crashes at 200)
 
 # convert to base path and str cat
@@ -73,6 +73,7 @@ x_test /= 255
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
+print(x_train)
 
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
@@ -81,16 +82,16 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 #mnist
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(11, 11),
+model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
-model.add(Conv2D(64, (11, 11), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
