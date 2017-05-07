6f4ae53c707e53e5b5e2e4425c5e2462caa8e1f5
Mike Lee <sukyungmlee@gmail.com>
2017-05-07 17:27:07 -0400

    modified fm_cnn_test to work with grayscale images

--- fm_cnn_test.py
+++ fm_cnn_test.py
@@ -19,7 +19,7 @@ import glob
 # convert to base path and str cat
 base_path = '../data/2/valid_processed_small2/'
 model_file = 'v2_fm.h5'
-num_chan = 3
+num_chan = 1
 
 num_images_orig = 10000
 num_aug = 20
@@ -32,7 +32,7 @@ input_shape = (img_rows, img_cols, num_chan)
 x = np.zeros((num_images_orig, img_rows, img_cols, num_chan))
 
 for ind in range(1, num_images_orig + 1):
-    img = cv2.imread(base_path + str(ind) + '.png', 1)
+    img = cv2.imread(base_path + str(ind) + '.png', 0)
     #print(img)
     x[ind - 1, :, :, :] = img
     #x_av = x_av + x[ind-1]
