11c331443e614113d0947a3166aed35221020689
Mike Lee <sukyungmlee@gmail.com>
2017-05-07 18:48:36 -0400

    created image preprocessor for fm that takes away noise background from validation images

--- preprocessImageClient_fm.m
+++ preprocessImageClient_fm.m
@@ -1,59 +1,63 @@
-close all
+% close all
+% 
+% % Simple test of preprocessImage()
+% im = imread('/home/msl/Dropbox/data/2/valid/13.png');
+% % figure
+% % imshow(im)
+% 
+% filter_row = 13;
+% filter_col = (filter_row + 1)/2 - 1;
+% threshold = 160;
+% 
+% denoisedImage = wiener2(im, [filter_row, filter_col]);
+% denoisedImage(denoisedImage > threshold) = 255;
+% denoisedImage(denoisedImage <= threshold) = 0;
+% figure
+% imshow((denoisedImage))
 
-% Simple test of preprocessImage()
+%%
 
-% im = imread('/home/msl/Dropbox/ML_project/data/1/train/right/2861.png');
-im = imread('/home/msl/Dropbox/ML_project/data/1/train/right/15.png');
+% Offline image preprocessor
+
+% profile on
+
+% define data path
+cd ..
+tempdir = pwd;
+cd ML_project
+rootFolder = fullfile(tempdir, 'data/2/valid');
+
+% for validation data
+imds = imageDatastore(rootFolder, 'LabelSource', 'foldernames');
+
+[numImages, ~] = size(imds.Files);
+% numImages = 200;
 tic
-im_rotated = preprocessImage(im);
+
+filter_row = 13;
+filter_col = (filter_row + 1)/2 - 1;
+threshold = 160;
+
+for j = 1:numImages
+    if mod(j,50) == 0
+        j
+    end
+    img_path = imds.Files{j};
+    im = imread(img_path);
+    denoisedImage = wiener2(im, [filter_row, filter_col]);
+    denoisedImage(denoisedImage > threshold) = 255;
+    denoisedImage(denoisedImage <= threshold) = 0;
+    %save_path = strrep(img_path, '/train/', '/train_bin/');
+    save_path = strrep(img_path, '/valid/', '/valid_bin/');
+    
+    imwrite(denoisedImage, save_path);
+end
 toc
-figure
-imshow(imbinarize(im_rotated))
-figure
-imshow(im_rotated)
 
 
-% %%
-% 
-% % Offline image preprocessor
-% 
-% % profile on
-% 
-% % define data path
-% cd ..
-% tempdir = pwd;
-% cd ML_project
-% rootFolder = fullfile(tempdir, 'data/1/valid');
-% 
-% % % for the left and right images
-% % categories = {'left', 'right'};
-% % imds = imageDatastore(fullfile(rootFolder, categories), 'LabelSource', 'foldernames');
-% 
-% % for validation data
-% imds = imageDatastore(rootFolder, 'LabelSource', 'foldernames');
-% 
-% [numImages, ~] = size(imds.Files);
-% % numImages = 200;
-% tic
-% for j = 1:numImages
-%     if mod(j,50) == 0
-%         j
-%     end
-%     img_path = imds.Files{j};
-%     im = imread(img_path);
-%     im_rotated = preprocessImage(im);
-%     im_rotated = imresize(im_rotated, [235 115]);  % resize image
-%     % save_path = strrep(img_path, '/train/', '/train/processed_small/');
-%     save_path = strrep(img_path, '/valid/', '/valid_processed_small/');
-%     
-%     imwrite(im_rotated, save_path);
-% end
-% toc
-% 
-% 
-% 
-% % imgPath = 'C:\Users\msl\Dropbox\ML_project\data\1\train\right\2861.png';
-% % im = imread(imgPath);
-% % im_rotated = preprocessImage(im);
-% % [height, width] = size(im_rotated);
-% % imwrite(im_rotated, strrep(imgPath, '\train\', '\train\processed\'));
+
+% imgPath = 'C:\Users\msl\Dropbox\ML_project\data\1\train\right\2861.png';
+% im = imread(imgPath);
+% im_rotated = preprocessImage(im);
+% [height, width] = size(im_rotated);
+% imwrite(im_rotated, strrep(imgPath, '\train\', '\train\processed\'));
